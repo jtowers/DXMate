2c9ae824b9b1202012ff827e95de2e140bc6e402
John Towers <jtowers@umail.iu.edu>
2017-09-05 17:35:34 -0400

    fix missing import

--- lib/diagnostic.py
+++ lib/diagnostic.py
@@ -20,6 +20,7 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
 
+import os
 from collections import OrderedDict
 import sublime
 from .util import util
